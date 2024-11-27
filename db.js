cad99b9aebd9b23ad207c1048daa612cdf033f6b
Louis Driffort <louis.driffort@gmail.com>
2024-11-27 09:30:25 +0100

    update render file and database url env variable name

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  process.env.POSTGRES_INTERNAL_URL,
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
